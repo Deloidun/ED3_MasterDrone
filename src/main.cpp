6115e8873b331b81b3861c3fed64a64691aac65d
LongDo1902 <dobaolong2002@gmail.com>
2023-12-05 11:40:09 +0700

    Adding screen for Master

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <Arduino.h>
 #include <Master_ESP_NOW.h>
+#include <mySSD1306.h>
 
 void setup()
 {
   // Set up UART 2 for Middle ESP32
   init_ESPNOW_Transmitter();
+  SSD1306_Setup(); //Screen setup
 }
 
 void loop()
 {
   // Sending UART data from Middle to Slave
+  DrawMrSonBitMap(); //Draw Mr Son
   sendingData_throughESPNOW();
 }
+
